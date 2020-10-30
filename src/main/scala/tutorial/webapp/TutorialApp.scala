2503643d252153096ee6b4e992c7105bbf54a226
Rupert Foggo McKay <rumckay@expediagroup.com>
2020-10-30 12:58:22 +0100

    No hardcoded button

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,10 +2,8 @@ package tutorial.webapp
 
 import org.scalajs.dom
 import org.scalajs.dom.document
-import scala.scalajs.js.annotation.JSExportTopLevel
 
 object TutorialApp {
-  @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
     appendPar(document.body, "You clicked the button!")
   }
@@ -16,7 +14,20 @@ object TutorialApp {
     targetNode.appendChild(parNode)
   }
 
+  def setupUI(): Unit = {
+    val button = document.createElement("button")
+    button.textContent = "Click me!"
+    button.addEventListener("click", { (e: dom.MouseEvent) =>
+      addClickedMessage()
+    })
+    document.body.appendChild(button)
+
+    appendPar(document.body, "Hello World")
+  }
+
   def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Hello world!")
+    document.addEventListener("DOMContentLoaded", { (e: dom.Event) =>
+      setupUI()
+    })
   }
 }
